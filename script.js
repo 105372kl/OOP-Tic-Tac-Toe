2de2aeedb5c3ec442dd22baf5a286dd105cee960
Kevin Le <106705824+105372kl@users.noreply.github.com>
2023-01-05 14:24:33 +0000

    implemented win checks and CPU first turn

--- script.js
+++ script.js
@@ -23,19 +23,24 @@ class tile {
 
 var gameboard = {
   turnNum: 0,
-  columnCheck: () => {
-    let column0 = [1,4,7]
-    let column1 = [2,5,8]
-    let column2 = [3,6,9]
-    let xCount = 0;
-    let oCount = 0;
+  check(...args) {
+    //console.log(args)
     for (let t = 0; t < 3; t++) {
+      let xCount = 0;
+      let oCount = 0;
       for (let i = 0; i < 3; i++) {
-        //let check = gameboard["tile"+(("column"+t)[i])][state]
-        //let test = "tile"+(("column"+t)[i])
-        //console.log(test)
-        //let check = (gameboard.test.state) //why doesnt accept second property in bracket notation
-        console.log(check)
+        //console.log(args[i])
+        function getTile () {
+          if (args.length > 1) {
+            return "tile"+(args[t][i])
+          }
+          else {
+            return "tile"+(args[0][i])
+          }
+        }
+        let tileCheck = getTile() //note to future self: eval() parses str into var
+        let check = gameboard[tileCheck].state
+        //console.log(check);
         if (check == "o") {
           oCount++
         }
@@ -43,8 +48,37 @@ var gameboard = {
           xCount++
         }
       }
+      if (oCount == 3) {
+        win();
+        break;
+      }
+      if (xCount == 3) {
+        win()
+        break;
+      }
+      //console.log("oCount "+oCount)
+      //console.log("xCount "+xCount)
     }
-    console.log(xCount)
+  },
+  columnCheck: () => {
+    let column0 = [1,4,7]
+    let column1 = [2,5,8]
+    let column2 = [3,6,9]
+    gameboard.check(column0, column1, column2)
+  },
+  rowCheck: () => {
+    let row0 = [1,2,3]
+    let row1 = [4,5,6]
+    let row2 = [7,8,9]
+    gameboard.check(row0, row1, row2)
+  },
+  fSlashCheck: () => {
+    let forwardSlash = [1,5,9]
+    gameboard.check(forwardSlash)
+  },
+  bSlashCheck: () => {
+    let backSlash = [3,5,7]
+    gameboard.check(backSlash)
   }
 }
 
@@ -57,30 +91,63 @@ function newBoard() {
 }
 
 function Turn() {
+  //console.log("columnCheck: ")
+  gameboard.columnCheck();
+  //console.log("rowCheck: ")
+  gameboard.rowCheck();
+  //console.log("fSlashCheck: ")
+  gameboard.fSlashCheck();
+  //console.log("bSlashCheck: ")
+  gameboard.bSlashCheck();
   if (gameboard.turnNum % 2 == 1) {
+    console.log("Turn: "+gameboard.turnNum);
     playerTurn();
+    return "player"
   }
-  else{
+  else if (gameboard.turnNum != -1) {
+    console.log("Turn: "+gameboard.turnNum);
     cpuTurn();
+    return "cpu"
   } 
-  console.log("Turn: "+gameboard.turnNum);
 }
 
 function firstTurn() {
-  gameboard.turnNum = Math.floor((Math.random() * 2) + 1);
-  Turn();
+  //gameboard.turnNum = Math.floor((Math.random() * 2) + 1);
+  gameboard.turnNum = 2;
+  if (Turn() == "cpu") {
+    let randomNum =  Math.floor((Math.random() * 9) + 1)
+    //console.log(randomNum)
+
+    //                  could place this into a function
+    let selectedTile = document.getElementById("tile"+randomNum)
+    selectedTile.innerHTML = "o"
+    gameboard["tile" + randomNum].state = "o";
+    //
+  }
+  else {Turn()}
 }
 
 function playerClick() {
-  this.innerHTML = "x";
-  //console.log(this)
-  for (let i = 1; i <= 9; i++) {
-    gameboard["tile" + i.toString()].button.removeEventListener("click", playerClick);
+  if (this.innerHTML == "") {
+    this.innerHTML = "x";
+    console.log(this)
+    for (let i = 1; i <= 9; i++) {
+      gameboard["tile" + i.toString()].button.removeEventListener("click", playerClick);
+    }
+    let value = this.value;
+    gameboard["tile" + value.toString()].state = "x";
+    gameboard.turnNum += 1;
+    Turn();
   }
-  let value = this.value;
-  gameboard["tile" + value.toString()].state = "x";
-  gameboard.turnNum += 1;
-  Turn();
+  else {
+    alert("Choose another tile")
+    //could make it so click event listener is added only to empty spaces
+  }
+}
+
+function win() {
+  alert("win");
+  gameboard.turnNum = -1;
 }
 
 function playerTurn() {
@@ -90,7 +157,6 @@ function playerTurn() {
 }
 
 function cpuTurn() {
-  //alert("hi")
   gameboard.turnNum += 1;
   Turn();
 }
@@ -101,13 +167,4 @@ function cpuTurn() {
 */
 
 newBoard()
-console.log(gameboard)
 firstTurn()
-gameboard.columnCheck()
-
-console.log(gameboard)
-
-//testing stuff
-
-/*i = 2
-gameboard["tile"+i].button.innerHTML = "x" */
