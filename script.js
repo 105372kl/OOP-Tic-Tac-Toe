8cf9290bdf87ce827c2a35a4c14e534c374254a9
Kevin Le <106705824+105372kl@users.noreply.github.com>
2023-01-09 18:14:27 +0000

    better win logic

--- script.js
+++ script.js
@@ -48,13 +48,25 @@ var gameboard = {
           xCount++
         }
       }
-      if (oCount == 3) {
-        win();
-        break;
+      switch (oCount) {
+        case 3:
+          return "win";
+        case 2:
+          //console.log("hi");
+          break;
+        case 1:
+          //console.log("hi");
+          break;
       }
-      if (xCount == 3) {
-        win()
-        break;
+      switch (xCount) {
+        case 3:
+          return "win";
+        case 2:
+          //console.log("hi");
+          break;
+        case 1:
+          //console.log("hi");
+          break;
       }
       //console.log("oCount "+oCount)
       //console.log("xCount "+xCount)
@@ -64,21 +76,21 @@ var gameboard = {
     let column0 = [1,4,7]
     let column1 = [2,5,8]
     let column2 = [3,6,9]
-    gameboard.check(column0, column1, column2)
+    return gameboard.check(column0, column1, column2)
   },
   rowCheck: () => {
     let row0 = [1,2,3]
     let row1 = [4,5,6]
     let row2 = [7,8,9]
-    gameboard.check(row0, row1, row2)
+    return gameboard.check(row0, row1, row2)
   },
   fSlashCheck: () => {
     let forwardSlash = [1,5,9]
-    gameboard.check(forwardSlash)
+    return gameboard.check(forwardSlash)
   },
   bSlashCheck: () => {
     let backSlash = [3,5,7]
-    gameboard.check(backSlash)
+    return gameboard.check(backSlash)
   }
 }
 
@@ -90,54 +102,45 @@ function newBoard() {
   }
 }
 
-function Turn() {
+function turn() {
   //console.log("columnCheck: ")
-  gameboard.columnCheck();
-  //console.log("rowCheck: ")
-  gameboard.rowCheck();
-  //console.log("fSlashCheck: ")
-  gameboard.fSlashCheck();
-  //console.log("bSlashCheck: ")
-  gameboard.bSlashCheck();
+  let conditionsArray = [
+    gameboard.columnCheck(),
+    gameboard.rowCheck(),
+    gameboard.fSlashCheck(),
+    gameboard.bSlashCheck(),
+  ]
+  if (conditionsArray.indexOf("win") != -1) {
+     win()
+  }
+  
   if (gameboard.turnNum % 2 == 1) {
     console.log("Turn: "+gameboard.turnNum);
     playerTurn();
-    return "player"
   }
   else if (gameboard.turnNum != -1) {
     console.log("Turn: "+gameboard.turnNum);
     cpuTurn();
-    return "cpu"
   } 
 }
 
 function firstTurn() {
   //gameboard.turnNum = Math.floor((Math.random() * 2) + 1);
-  gameboard.turnNum = 2;
-  if (Turn() == "cpu") {
-    let randomNum =  Math.floor((Math.random() * 9) + 1)
-    //console.log(randomNum)
-
-    //                  could place this into a function
-    let selectedTile = document.getElementById("tile"+randomNum)
-    selectedTile.innerHTML = "o"
-    gameboard["tile" + randomNum].state = "o";
-    //
-  }
-  else {Turn()}
+  gameboard.turnNum = 1;
+  turn();
 }
 
 function playerClick() {
   if (this.innerHTML == "") {
     this.innerHTML = "x";
-    console.log(this)
+    //console.log(this)
     for (let i = 1; i <= 9; i++) {
       gameboard["tile" + i.toString()].button.removeEventListener("click", playerClick);
     }
     let value = this.value;
     gameboard["tile" + value.toString()].state = "x";
     gameboard.turnNum += 1;
-    Turn();
+    turn();
   }
   else {
     alert("Choose another tile")
@@ -157,8 +160,42 @@ function playerTurn() {
 }
 
 function cpuTurn() {
+  if (gameboard.turnNum == 2) {
+    randomMove();
+  }
+  let randomNum = Math.floor((Math.random() * 4) + 1)
+  switch (randomNum) {
+    case 1:
+      cpuCheck("columnCheck()")
+      break;
+    case 2:
+      cpuCheck("rowCheck()")
+      break;
+    case 3:
+      cpuCheck("fSlashCheck()")
+      break;
+    case 4:
+      cpuCheck("bSlashCheck()")
+      break;
+  }
   gameboard.turnNum += 1;
-  Turn();
+  turn();
+}
+
+function cpuCheck(randomcheck) {
+  console.log("gameboard."+randomcheck)
+}
+
+function randomMove() {
+  let randomNum =  Math.floor((Math.random() * 9) + 1)
+  let selectedTile = document.getElementById("tile"+randomNum)
+  if (gameboard["tile" + randomNum].state == "-") {
+    selectedTile.innerHTML = "o"
+    gameboard["tile" + randomNum].state = "o";
+  }
+  else {
+    randomMove()
+  }
 }
 
 
