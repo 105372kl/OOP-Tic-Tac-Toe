51d6406ae1bff4a9defe286f87ed6b1f95651c8b
Kevin Le <106705824+105372kl@users.noreply.github.com>
2023-01-10 19:19:34 +0000

    fill gap in progress, though win conditions broken?

--- script.js
+++ script.js
@@ -25,50 +25,73 @@ var gameboard = {
   turnNum: 0,
   check(...args) {
     //console.log(args)
-    for (let t = 0; t < 3; t++) {
-      let xCount = 0;
-      let oCount = 0;
-      for (let i = 0; i < 3; i++) {
-        //console.log(args[i])
-        function getTile() {
-          if (args.length > 1) {
-            return "tile" + (args[t][i])
+    if (args[0] == true) {
+      alert("hi")
+      args.shift();
+       for (let t = 0; t < 3; t++) {
+        for (let i = 0; i < 3; i++) {
+          //console.log(args[i])
+          function getTile() {
+            if (args.length > 1) {
+              return "tile" + (args[t][i])
+            }
+            else {
+              return "tile" + (args[0][i])
+            }
           }
-          else {
-            return "tile" + (args[0][i])
+          let tileCheck = getTile() //note to future self: eval() parses str into var
+          let check = gameboard[tileCheck].state
+        }
+       }
+    }
+    else {
+      args.shift();
+      for (let t = 0; t < 3; t++) {
+        let xCount = 0;
+        let oCount = 0;
+        for (let i = 0; i < 3; i++) {
+          //console.log(args[i])
+          function getTile() {
+            if (args.length > 1) {
+              return "tile" + (args[t][i])
+            }
+            else {
+              return "tile" + (args[0][i])
+            }
+          }
+          let tileCheck = getTile() //note to future self: eval() parses str into var
+          let check = gameboard[tileCheck].state
+          //console.log(check);
+          if (check == "o") {
+            oCount++
+          }
+          else if (check == "x") {
+            xCount++
           }
         }
-        let tileCheck = getTile() //note to future self: eval() parses str into var
-        let check = gameboard[tileCheck].state
-        //console.log(check);
-        if (check == "o") {
-          oCount++
+        switch (oCount) {
+          case 3:
+            return "win";
+          case 2:
+            //console.log("hi");
+            return "o2";
+          case 1:
+            //console.log("hi");
+            return "o1";
         }
-        else if (check == "x") {
-          xCount++
+        switch (xCount) {
+          case 3:
+            return "win";
+          case 2:
+            //console.log("hi");
+            return "x2";
+          case 1:
+            //console.log("hi");
+            return "x1";
         }
-      }
-      switch (oCount) {
-        case 3:
-          return "win";
-        case 2:
-          //console.log("hi");
-          return "o2";
-        case 1:
-          //console.log("hi");
-          return "o1";
-      }
-      switch (xCount) {
-        case 3:
-          return "win";
-        case 2:
-          //console.log("hi");
-          return "x2";
-        case 1:
-          //console.log("hi");
-          return "x1";
       }
     }
+
     //console.log("oCount "+oCount)
     //console.log("xCount "+xCount)
   },
@@ -76,25 +99,35 @@ var gameboard = {
   column1: [2, 5, 8],
   column2: [3, 6, 9],
 
-  columnCheck: () => {
-    return gameboard.check(gameboard.column0, gameboard.column1, gameboard.column2)
+  columnCheck: (fillGap = false) => {
+    return gameboard.check(
+      fillGap,
+      gameboard.column0,
+      gameboard.column1,
+      gameboard.column2
+    )
   },
   row0: [1, 2, 3],
   row1: [4, 5, 6],
   row2: [7, 8, 9],
 
-  rowCheck: () => {
-    return gameboard.check(gameboard.row0, gameboard.row1, gameboard.row2)
+  rowCheck: (fillGap = false) => {
+    return gameboard.check(
+      fillGap,
+      gameboard.row0,
+      gameboard.row1,
+      gameboard.row2
+    )
   },
   forwardSlash: [1, 5, 9],
 
-  fSlashCheck: () => {
-    return gameboard.check(gameboard.forwardSlash)
+  fSlashCheck: (fillGap = false) => {
+    return gameboard.check(fillGap, gameboard.forwardSlash)
   },
   backSlash: [3, 5, 7],
 
-  bSlashCheck: () => {
-    return gameboard.check(gameboard.backSlash)
+  bSlashCheck: (fillGap = false) => {
+    return gameboard.check(fillGap, gameboard.backSlash)
   }
 }
 
@@ -177,28 +210,28 @@ function randomCheck() {
   let randomNum = Math.floor((Math.random() * 4) + 1);
   switch (randomNum) {
     case 1:
-      cpuCheck("columnCheck()")
+      cpuCheck("columnCheck(")
       break;
     case 2:
-      cpuCheck("rowCheck()")
+      cpuCheck("rowCheck(")
       break;
     case 3:
-      cpuCheck("fSlashCheck()")
+      cpuCheck("fSlashCheck(")
       break;
     case 4:
-      cpuCheck("bSlashCheck()")
+      cpuCheck("bSlashCheck(")
       break;
   }
 }
 
 function cpuCheck(check) {
   console.log(check)
-  switch (eval("gameboard." + check)) {
+  switch (eval("gameboard." + check + ")")) {
     case "o2":
-      fillGap(check)
+      cpuFillGap(check)
       break;
     case "x2":
-      alert("x2")
+      cpuFillGap(check)
       break;
     case "o1":
       break;
@@ -207,8 +240,11 @@ function cpuCheck(check) {
   }
 }
 
-function fillGap(check) {
-  eval("gameboard." + check)
+function cpuFillGap(check) {
+  let fillGap = true;
+  let str = "gameboard." + check + fillGap + ")";
+  eval(str)
+
 }
 
 function randomMove() {
